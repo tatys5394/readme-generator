c8b755b00d8028ec7821b7284f5610b3e9cfc253
tatys5394 <123910162+tatys5394@users.noreply.github.com>
2023-03-01 16:50:29 -0500

    added a question, markdown, and write file

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,40 @@
+// DEPENDENCIES
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
+// DATA
 // TODO: Create an array of questions for user input
-const questions = [];
+// /* Pass your questions in here */
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
 
+const questions = [
+  // title
+  {
+    type: "input",
+    message: "What's the tile of your README?",
+    name: "title",
+  },
+  // description
+  {
+    type: "input",
+    message: "Write a description of your assignment",
+    name: "description",
+  },
+  // installation instructions
+];
+
+// FUNCTIONS
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function generateMarkdown(answers) {
+  const markdown = `# ${answers.title} `
+  console.log(markdown);
+  return markdown;
+}
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName,data,(err) => err ? console.error(err) : console.log('Success!'));
+}
 
 // TODO: Create a function to initialize app
 function init() {}
@@ -12,15 +42,20 @@ function init() {}
 // Function call to initialize app
 init();
 
-
-var inquirer = require('inquirer');
+// USER INTERACTION
 inquirer
-  .prompt([
-    /* Pass your questions in here */
-  ])
+  .prompt([...questions])
   .then((answers) => {
+    // console.log(answers);
+    // generate some markdown with our answers
+
+    const markdown = generateMarkdown(answers);
+    console.log("this should be markdown:", markdown);
+    writeToFile("README.md",markdown)
+
     // Use user feedback for... whatever!!
   })
+  
   .catch((error) => {
     if (error.isTtyError) {
       // Prompt couldn't be rendered in the current environment
@@ -28,3 +63,5 @@ inquirer
       // Something else went wrong
     }
   });
+
+// INITIALIZATION
